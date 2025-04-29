495158cd4f5b49916ce0bd39f6a7144d383c3ea2
Mikey- Desktop <mikeyhardy14@gmail.com>
2025-04-29 17:31:50 -0400

    Fixed favicon.ico

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,12 @@
-import type { Metadata } from "next";
 import "./globals.css";
 import NavBar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
 
-export const metadata: Metadata = {
-  title: "Jupiter Marketing",
-  description: "Taking your ideas to new heights",
-  icons: {
-    icon: "/images/logo.png",
-  },
-};
-
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
       <body>
